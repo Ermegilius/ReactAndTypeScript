aef7fe7f8eb6bd87fe3d8f25205d400f536ed6cc
Vladimir Beliakov <ermegilius@gmail.com>
2025-01-15 16:45:04 +0200

    finish lesson 19

--- README.md
+++ README.md
@@ -35,28 +35,53 @@ hobbies = ["Sports", "Cooking", "Reading"];
 
 ```ts
 function add(a: number, b: number): void {
-    // void for a function with no return value
-    const result = a + b;
-    console.log(result);
+	// void for a function with no return value
+	const result = a + b;
+	console.log(result);
 }
 
 function add2(a: number, b: number): number {
-    // and here we return number
-    const result = a + b;
-    return result;
+	// and here we return number
+	const result = a + b;
+	return result;
 }
 ```
 
 ## Callback types
 
 ```ts
-function calculate(
-    a: number,
-    b: number,
-    calcFn: (a: number, b: number) => number
-) {
-    calcFn(a, b);
+function calculate(a: number, b: number, calcFn: (a: number, b: number) => number) {
+	calcFn(a, b);
 }
 
 calculate(2, 5, add2); //call it with add2 function
+```
+
+## Custom types
+
+```ts
+type StringOrNumber = string | number;
+let userId: StringOrNumber = "abc1";
+userId = 123;
+```
+
+```ts
+type User = {
+	name: string;
+	age: number;
+	isAdmin: boolean;
+	id: StringOrNumber;
+};
+let user: User;
+```
+
+## Interface
+
+Usially is used to define object types.
+
+```ts
+interface Credentials {
+	user: string;
+	pass: string;
+}
 ```
--- first-app.ts
+++ first-app.ts
@@ -7,26 +7,30 @@ userName = "Manu"; // OK
 let userAge = 34;
 let isValid = true;
 
-let userId: string | number = "abc1";
+type StringOrNumber = string | number;
+let userId: StringOrNumber = "abc1";
 userId = 123;
 
 //userId = true;
 
 //let user: object;
-let user: {
-    name: string;
-    age: number;
-    isAdmin: boolean;
-    id: string | number;
+
+type User = {
+	name: string;
+	age: number;
+	isAdmin: boolean;
+	id: StringOrNumber;
 };
+
+let user: User;
 
 //user = 'Max';
 
 user = {
-    name: "Max",
-    age: 34,
-    isAdmin: true,
-    id: "abc", // 123
+	name: "Max",
+	age: 34,
+	isAdmin: true,
+	id: "abc", // 123
 };
 
 //user = {};
@@ -38,24 +42,36 @@ hobbies = ["Sports", "Cooking", "Reading"];
 //hobbies = [1,2,3]; //error type
 
 function add(a: number, b: number): void {
-    // void for a function with no return value
-    const result = a + b;
-    console.log(result);
+	// void for a function with no return value
+	const result = a + b;
+	console.log(result);
 }
 
 function add2(a: number, b: number): number {
-    // and here we return number
-    const result = a + b;
-    return result;
+	// and here we return number
+	const result = a + b;
+	return result;
 }
 
-function calculate(
-    a: number,
-    b: number,
-    calcFn: (a: number, b: number) => number
-) {
-    calcFn(a, b);
-    console.log(calcFn(a, b));
+type AddFn = (a: number, b: number) => number;
+
+function calculate(a: number, b: number, calcFn: AddFn) {
+	calcFn(a, b);
+	console.log(calcFn(a, b));
 }
 
 calculate(2, 5, add2);
+
+interface Credentials {
+	user: string;
+	password: string;
+	email: string;
+}
+
+let creds: Credentials;
+
+creds = {
+	user: "Max",
+	password: "123",
+	email: "wsedrfg",
+};
