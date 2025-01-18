bc744aba897d618b63de641a30b091da520a31e2
Vladimir Beliakov <ermegilius@gmail.com>
2025-01-18 11:10:52 +0200

    finish lesson 39. Add: new components

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import CourseGoal from "./components/CourseGoal.tsx";
 import Header from "./components/Header.tsx";
+import CourseGoalsList from "./components/CourseGoalsList.tsx";
 import goalsImg from "./assets/goals.jpg";
+import { useState } from "react";
 
-type CourseGoal = {
+export type CourseGoal = {
 	title: string;
 	description: string;
 	id: number;
@@ -21,22 +21,13 @@ export default function App() {
 			return [...prevGoals, newGoal];
 		});
 	}
-
 	return (
 		<main>
 			<Header image={{ src: goalsImg, alt: "A list of goals" }}>
 				<h1>Your course goals</h1>
 			</Header>
 			<button onClick={handleAddGoal}>Add Goal</button>
-			<ul>
-				{goals.map((goal) => (
-					<li key={goal.id}>
-						<CourseGoal title={goal.title}>
-							<p>{goal.description}</p>
-						</CourseGoal>
-					</li>
-				))}
-			</ul>
+			<CourseGoalsList goals={goals} />
 		</main>
 	);
 }
