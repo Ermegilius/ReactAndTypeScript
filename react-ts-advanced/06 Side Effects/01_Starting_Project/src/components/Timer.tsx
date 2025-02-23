ef900f1df1b21540d6469d20625b58e562e5026f
Vladimir Beliakov <ermegilius@gmail.com>
2025-02-23 11:06:08 +0200

    end of lesson 77

--- react-ts-advanced/06 Side Effects/01_Starting_Project/src/components/Timer.tsx
+++ react-ts-advanced/06 Side Effects/01_Starting_Project/src/components/Timer.tsx
@@ -1,14 +1,21 @@
 import Container from "./UI/Container.tsx";
 import { type Timer as TimerProps } from "../store/timers-context.tsx";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Timer({ name, duration }: TimerProps) {
+	const interval = useRef<number | null>(null);
 	const [remainingTime, setRemainingTime] = useState(duration * 1000); //user sets s; logic uses ms
-
+	if (remainingTime <= 0 && interval.current) {
+		clearInterval(interval.current);
+	}
 	useEffect(() => {
-		setInterval(function () {
+		const timer = setInterval(function () {
 			setRemainingTime((prevTime) => prevTime - 10);
 		}, 10);
+		interval.current = timer;
+		return () => {
+			clearInterval(timer);
+		};
 	}, []);
 
 	const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
