15322a1988872eff9fa26ad2fd3c910bd72aa3ed
Vladimir Beliakov <ermegilius@gmail.com>
2025-02-23 12:32:39 +0200

    add: zod library for fetch types validation

--- react-ts-advanced/06 Side Effects/04_New_Project_Starting_Project/src/App.tsx
+++ react-ts-advanced/06 Side Effects/04_New_Project_Starting_Project/src/App.tsx
@@ -2,29 +2,42 @@ import BlogPosts, { BlogPost } from "./components/BlogPosts";
 import { get } from "./util/http";
 import { type ReactNode, useEffect, useState } from "react";
 import fetchingImage from "./assets/data-fetching.png";
+import { z } from "zod";
 
-type RawDataBlogPost = {
-	id: number;
-	useId: number;
-	title: string;
-	body: string;
-};
+const rawDataBlogPostSchema = z.object({
+	id: z.number(),
+	userId: z.number(),
+	title: z.string(),
+	body: z.string(),
+});
+
+const expectedResponseDataSchema = z.array(rawDataBlogPostSchema);
 
 function App() {
 	const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+	const [isFetching, setIsFetching] = useState(false);
+	const [error, setError] = useState<string | null>(null);
 
 	useEffect(() => {
 		async function fetchPosts() {
-			const data = (await get("https://jsonplaceholder.typicode.com/posts")) as RawDataBlogPost[];
-
-			const blogPosts: BlogPost[] = data.map((rawPost) => {
-				return {
-					id: rawPost.id,
-					title: rawPost.title,
-					text: rawPost.body,
-				};
-			});
-			setFetchedPosts(blogPosts);
+			setIsFetching(true);
+			try {
+				const data = await get("https://jsonplaceholder.typicode.com/posts");
+				const parsedData = expectedResponseDataSchema.parse(data);
+				const blogPosts: BlogPost[] = parsedData.map((rawPost) => {
+					return {
+						id: rawPost.id,
+						title: rawPost.title,
+						text: rawPost.body,
+					};
+				});
+				setFetchedPosts(blogPosts);
+			} catch (error) {
+				if (error instanceof Error) {
+					setError(error.message);
+				}
+			}
+			setIsFetching(false);
 		}
 		fetchPosts();
 	}, []);
@@ -36,6 +49,9 @@ function App() {
 
 	return (
 		<main>
+			<h1>Blog Posts</h1>
+			{isFetching && <p>Loading...</p>}
+			{error && <p style={{ color: "red" }}>{error}</p>}
 			<img src={fetchingImage} alt="An abstract image depicting a data fetching" />
 			{content}
 		</main>
