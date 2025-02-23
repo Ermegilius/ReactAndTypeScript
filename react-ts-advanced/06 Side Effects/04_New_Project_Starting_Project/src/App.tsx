9e7ea866aec220cd1acd1b2f63eb81b70c37f0de
Vladimir Beliakov <ermegilius@gmail.com>
2025-02-23 12:15:20 +0200

    end of lesson 82

--- react-ts-advanced/06 Side Effects/04_New_Project_Starting_Project/src/App.tsx
+++ react-ts-advanced/06 Side Effects/04_New_Project_Starting_Project/src/App.tsx
@@ -1,5 +1,45 @@
+import BlogPosts, { BlogPost } from "./components/BlogPosts";
+import { get } from "./util/http";
+import { type ReactNode, useEffect, useState } from "react";
+import fetchingImage from "./assets/data-fetching.png";
+
+type RawDataBlogPost = {
+	id: number;
+	useId: number;
+	title: string;
+	body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+	const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+	useEffect(() => {
+		async function fetchPosts() {
+			const data = (await get("https://jsonplaceholder.typicode.com/posts")) as RawDataBlogPost[];
+
+			const blogPosts: BlogPost[] = data.map((rawPost) => {
+				return {
+					id: rawPost.id,
+					title: rawPost.title,
+					text: rawPost.body,
+				};
+			});
+			setFetchedPosts(blogPosts);
+		}
+		fetchPosts();
+	}, []);
+
+	let content: ReactNode;
+	if (fetchedPosts) {
+		content = <BlogPosts posts={fetchedPosts} />;
+	}
+
+	return (
+		<main>
+			<img src={fetchingImage} alt="An abstract image depicting a data fetching" />
+			{content}
+		</main>
+	);
 }
 
 export default App;
