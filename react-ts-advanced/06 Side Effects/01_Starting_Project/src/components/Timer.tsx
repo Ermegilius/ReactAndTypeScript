e5d7cb090b0338829275b7a331bc622c3c2ee6bf
Vladimir Beliakov <ermegilius@gmail.com>
2025-02-23 10:47:46 +0200

    end of lesson 76

--- react-ts-advanced/06 Side Effects/01_Starting_Project/src/components/Timer.tsx
+++ react-ts-advanced/06 Side Effects/01_Starting_Project/src/components/Timer.tsx
@@ -1,20 +1,25 @@
 import Container from "./UI/Container.tsx";
 import { type Timer as TimerProps } from "../store/timers-context.tsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Timer({ name, duration }: TimerProps) {
 	const [remainingTime, setRemainingTime] = useState(duration * 1000); //user sets s; logic uses ms
-	setInterval(function () {
-		setRemainingTime();
-	}, 50);
+
+	useEffect(() => {
+		setInterval(function () {
+			setRemainingTime((prevTime) => prevTime - 10);
+		}, 10);
+	}, []);
+
+	const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
 	return (
 		<Container as="article">
 			<h2>{name}</h2>
 			<p>
-				<progress />
+				<progress max={duration * 1000} value={remainingTime} />
 			</p>
-			<p>{}</p>
+			<p>{formattedRemainingTime}</p>
 		</Container>
 	);
 }
